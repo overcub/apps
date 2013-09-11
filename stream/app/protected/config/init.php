56425142d4accea80e6af8cb4de6c4e6d6b87e24
root <root@glcapp01.(none)>
2013-09-11 23:52:05 +0000

    alteracoes do ar

--- stream/app/protected/config/init.php
+++ stream/app/protected/config/init.php
@@ -3,10 +3,10 @@
 DEFINE('AMBIENT_AIR',1);
 DEFINE('AMBIENT_LOCAL',2);
 $CONFIG = array();
-$ambient = AMBIENT_LOCAL;
+$ambient = AMBIENT_AIR;
 
 switch($ambient){
-    case AMBIENT_LOCAL:
+    case AMBIENT_AIR:
         $CONFIG['database'][1]['connectionString'] = 'mysql:host=192.168.1.100;dbname=apps.geeklifeclub';
         $CONFIG['database'][1]['username'] = 'geeklifeclub';
         $CONFIG['database'][1]['password'] = 'geeklifeclubaabc33163356';
@@ -18,8 +18,8 @@ switch($ambient){
         
         $CONFIG['params']['googleAnalyticsId'] = '';
         $CONFIG['params']['googleAnalyticsDomain'] = '';
-        $CONFIG['params']['cdnUrl'] = 'http://local.stream.geeklifeclub.com.br';
-        $CONFIG['params']['defaultUrl'] = 'http://local.stream.geeklifeclub.com.br';
+        $CONFIG['params']['cdnUrl'] = 'http://stream.geeklifeclub.com.br';
+        $CONFIG['params']['defaultUrl'] = 'http://stream.geeklifeclub.com.br';
 
         $CONFIG['params']['assetManager'] = array(
         		'class' => 'ext.version-asset-manager.VersionAssetManager',
@@ -28,20 +28,20 @@ switch($ambient){
 
     break;
 
-    case AMBIENT_AIR:
-        $CONFIG['database'][1]['connectionString'] = 'mysql:host=localhost;dbname=apps.geeklifeclub';
-        $CONFIG['database'][1]['username'] = 'geek';
-        $CONFIG['database'][1]['password'] = 'p3rs0n4xky';
-        $CONFIG['database'][2]['connectionString'] = 'mysql:host=localhost;dbname=apps.geeklifeclub';
-        $CONFIG['database'][2]['username'] = 'geek';
-        $CONFIG['database'][2]['password'] = 'p3rs0n4xky';
+    case AMBIENT_LOCAL:
+        $CONFIG['database'][1]['connectionString'] = 'mysql:host=192.241.128.122;dbname=apps.geeklifeclub';
+        $CONFIG['database'][1]['username'] = 'geeklifeclub';
+        $CONFIG['database'][1]['password'] = 'geeklifeclubaabc33163356';
+        $CONFIG['database'][2]['connectionString'] = 'mysql:host=192.241.128.122;dbname=apps.geeklifeclub';
+        $CONFIG['database'][2]['username'] = 'geeklifeclub';
+        $CONFIG['database'][2]['password'] = 'geeklifeclubaabc33163356';
         
         $CONFIG['cache'][1]['host'] = 'localhost';
         
         $CONFIG['params']['googleAnalyticsId'] = '';
         $CONFIG['params']['googleAnalyticsDomain'] = '';
-        $CONFIG['params']['cdnUrl'] = 'http://local.stream.geeklifeclub.com.br';
-        $CONFIG['params']['defaultUrl'] = 'http://local.stream.geeklifeclub.com.br';
+        $CONFIG['params']['cdnUrl'] = 'http://stream.geeklifeclub.com.br';
+        $CONFIG['params']['defaultUrl'] = 'http://stream.geeklifeclub.com.br';
 
         $CONFIG['params']['assetManager'] = array(
                 'class' => 'ext.version-asset-manager.VersionAssetManager',
