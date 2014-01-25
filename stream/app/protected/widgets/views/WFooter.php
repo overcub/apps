1e8df415a2f52e8c89b955b69d260493ef0f9154
Thiago Quintino de Paiva <thiag.q@gmail.com>
2014-01-25 12:09:24 -0200

    Finalizando a nova home do manete.tv

--- stream/app/protected/widgets/views/WFooter.php
+++ stream/app/protected/widgets/views/WFooter.php
@@ -18,23 +18,26 @@
 		<div class="row-fluid">
 			<div class="span4">
 				<article>
-					<img src="/images/style/icon/ico-streaming.png" alt="Mostre aos seus amigos!">
-					<h1>Streaming</h1>
-					<p>Mostre aos seus amigos como se joga</p>
+					<img src="/images/style/icon/ico-stream.png" alt="Mostre aos seus amigos!">
+					<h1>Faça sua stream</h1>
+					<p>Mostre a comunidade como você joga!
+					Utilize sua conta do twitch.tv e tenha todos os benefícios da nossa plataforma!</p>
 				</article>
 			</div>
 			<div class="span4">
 				<article>
 					<img src="/images/style/icon/ico-assista.png" alt="Assista aos melhores jogos!">
-					<h1>Assista</h1>
-					<p>Assista aos melhores jogos da atualidade</p>
+					<h1>Viewers</h1>
+					<p>Quanto mais views você tiver, mais capital você vai gerar!
+						No manete.tv nós trabalhamos para conseguir anunciantes para o seu canal.</p>
 				</article>
 			</div>
 			<div class="span4">
 				<article>
-					<img src="/images/style/icon/ico-divulgue.png" alt="Ganhe prêmios e recompensas por suas streams!">
-					<h1>Divulgue</h1>
-					<p>Divulgue o seu streaming e ganhe vantagens</p>
+					<img src="/images/style/icon/ico-money.png" alt="Ganhe prêmios e recompensas por suas streams!">
+					<h1>Receba</h1>
+					<p>Receba o valor gerado por suas streams!
+						70% de todo o valor gerado vai para você!</p>
 				</article>
 			</div>
 		</div>
@@ -42,9 +45,12 @@
 </section>
 <footer class="glc-footer">
 	<div class="container">
-		<p clas="about">
+		<p class="about pull-left">
 		Copyright &copy; <?php echo date('Y'); ?> by Geek Life Club.<br/>
 		All Rights Reserved.<br/>
+		</p>
+		<p class="pull-right">
+			<img src="/images/logo/logo-manete-tv-full.png" alt="manete.tv - by Geek Life Club">
 		</p>
 	</div>	
 </footer>
