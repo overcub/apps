82ace27bf775d6126e0e6b57c946f064eca90e36
rtancman <rtancman@gmail.com>
2014-01-30 21:42:25 -0200

    aplicando menu

--- stream/app/protected/widgets/views/WHeader.php
+++ stream/app/protected/widgets/views/WHeader.php
@@ -56,22 +56,23 @@
             <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
             <div class="nav-collapse collapse">
               <ul class="nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <!-- Read about Bootstrap dropdowns at http://twbs.github.com/bootstrap/javascript.html#dropdowns -->
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li class="nav-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
+                <li class="active"><a href="/">Home</a></li>
+                <li><a href="/sobre">Sobre</a></li>
+                <?php if( !Yii::app()->user->isGuest ): ?>
+                	<!-- Read about Bootstrap dropdowns at http://twbs.github.com/bootstrap/javascript.html#dropdowns -->
+	                <li class="dropdown">
+	                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img border="0" align="absmiddle" src="<?php echo (isset(Yii::app()->user->img) )?Yii::app()->user->img:"00000000" ?>&s=25" alt="<?php echo Yii::app()->user->name ?>" /> <b class="caret"></b></a>
+	                  <ul class="dropdown-menu">
+	                    <li><a href="/usuario/editar">Editar</a></li>
+	                    <li><a href="/stream/<?php echo Yii::app()->user->name ?>">Meu Stream</a></li>
+	                    <li class="divider"></li>
+	                    <li><a href="/logout">Sair</a></li>
+	                  </ul>
+	                </li>
+            	<?php else: ?>
+            		<li><a href="/login">Login</a></li>
+            		<li><a href="/cadastro">Cadastro</a></li>
+                <?php endif; ?>
               </ul>
             </div><!--/.nav-collapse -->
           </div><!-- /.navbar-inner -->
