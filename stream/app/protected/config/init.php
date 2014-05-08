0f8018f68abfd3486d3eec695991aab1f4c77b32
Thiago Quintino de Paiva <thiag.q@gmail.com>
2014-05-08 20:01:05 -0300

    Alteracoes de ip

--- stream/app/protected/config/init.php
+++ stream/app/protected/config/init.php
@@ -3,7 +3,7 @@
 DEFINE('AMBIENT_AIR',1);
 DEFINE('AMBIENT_LOCAL',2);
 $CONFIG = array();
-$ambient = AMBIENT_AIR;
+$ambient = AMBIENT_LOCAL;
 
 switch($ambient){
     case AMBIENT_AIR:
@@ -30,10 +30,10 @@ switch($ambient){
     break;
 
     case AMBIENT_LOCAL:
-        $CONFIG['database'][1]['connectionString'] = 'mysql:host=192.168.1.109;dbname=apps.geeklifeclub';
+        $CONFIG['database'][1]['connectionString'] = 'mysql:host=172.20.10.3;dbname=apps.geeklifeclub';
         $CONFIG['database'][1]['username'] = 'geeklifeclub';
         $CONFIG['database'][1]['password'] = 'geeklifeclubaabc33163356';
-        $CONFIG['database'][2]['connectionString'] = 'mysql:host=192.168.1.109;dbname=apps.geeklifeclub';
+        $CONFIG['database'][2]['connectionString'] = 'mysql:host=172.20.10.3;dbname=apps.geeklifeclub';
         $CONFIG['database'][2]['username'] = 'geeklifeclub';
         $CONFIG['database'][2]['password'] = 'geeklifeclubaabc33163356';
         
