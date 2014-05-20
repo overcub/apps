1d12c63db2390aeeb9a1f22399d6a488c1d72606
Thiago Quintino de Paiva <thiag.q@gmail.com>
2014-05-20 15:21:06 -0300

    Finalizando alteracoes para a pagina do streamer

--- stream/app/protected/views/players/streaming.php
+++ stream/app/protected/views/players/streaming.php
@@ -50,6 +50,7 @@ body{
     background-color: #E8E8E8;
     padding-left: 5px;
     padding-right: 5px;
+    padding-top: 10px;
 }
 .container-full .man-chat {
     background-color: #262126;
@@ -65,6 +66,28 @@ body{
     white-space: nowrap;
     width: 338px;
     z-index: 900;
+}
+.social-bottom .button.facebook {
+	position: relative;
+	top: -4px;
+}
+.streamer-data {
+}
+.streamer-data .avatar {
+	display: inline-block;
+	vertical-align: top;
+}
+.streamer-data .stream-name {
+	display: inline-block;
+	vertical-align: top;
+}
+.streamer-data .stream-name .user {
+	font-size: 20px;
+	margin-bottom: 0;
+}
+.streamer-data .stream-name .game {
+	font-size: 14px;
+    margin-bottom: 0;
 }
 	</style>
 	
@@ -90,12 +113,42 @@ body{
 					</div>
 					<div class="man-player-profile">
 						<div class="row-fluid">
-							<div class="span8">
+							<div class="span9">
+								<div class="streamer-data">
+									<div class="avatar">
+										<img border="0" class="pull-left" src="<?php echo $model->getImageProfile(56) ?>" alt="<?php echo $model->name ?>"/>
+									</div>
+									<div class="stream-name">
+										<p class="user"><strong>USUARIO</strong> - Streamando de boa aqui</p>
+										<p class="game">League of Legends</p>
+									</div>
+								</div>
+							</div>
+							<div class="span3">
+								<?php $this->widget('yii-sharrre.widgets.WSocialShare', array(
+										'htmlOptions' => array('id' => 'social-top-container','class'=>'demo5 pull-left'),
+										'elementId' => 'social-bottom',
+										'dataUrl' => $this->createAbsoluteUrl('/stream/'.$model->username),
+										'socials' => array(
+										'facebook' => true,
+										'googlePlus' => true,
+										'twitter' => true,
+										'digg' => false,
+										'delicious' => false,
+										'stumbleupon' => false,
+										'linkedin' => false,
+										'pinterest' => false
+									),
+								)); ?>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="span12">
 								<?php if($lolPlayer instanceof LolPlayer ) :?>
 								    <table class="table">
 							          <thead>
 							            <tr>
-							              <th><img border="0" class="pull-left" src="<?php echo $model->getImageProfile(56) ?>" alt="<?php echo $model->name ?>"/></th>
+							              <th></th>
 							              <th>Level</th>
 							              <th>Server</th>
 							              <th>Prestativo</th>
@@ -119,33 +172,16 @@ body{
 							        </table>
 								<?php endif ?>
 							</div>
-							<div class="span2 pull-right">
-							<?php $this->widget('yii-sharrre.widgets.WSocialShare', array(
-									'htmlOptions' => array('id' => 'social-top-container','class'=>'demo5 pull-left'),
-									'elementId' => 'social-bottom',
-									'dataUrl' => $this->createAbsoluteUrl('/stream/'.$model->username),
-									'socials' => array(
-									'facebook' => true,
-									'googlePlus' => true,
-									'twitter' => true,
-									'digg' => false,
-									'delicious' => false,
-									'stumbleupon' => false,
-									'linkedin' => false,
-									'pinterest' => false
-								),
-							)); ?>
-							</div>
 						</div>
 						<div class="row-fluid">
 			<ul class="nav nav-tabs" id="myTab">
-              <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
-              <li class=""><a data-toggle="tab" href="#profile">Profile</a></li>
+              <li class="active"><a data-toggle="tab" href="#home">Meu Perfil</a></li>
+              <li class=""><a data-toggle="tab" href="#profile">Todos os Vídeos</a></li>
               <li class="dropdown">
-                <a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <b class="caret"></b></a>
+                <a data-toggle="dropdown" class="dropdown-toggle" href="#">Siga-me <b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                  <li><a data-toggle="tab" href="#dropdown1">@fat</a></li>
-                  <li><a data-toggle="tab" href="#dropdown2">@mdo</a></li>
+                  <li><a href="www.twitter.com">Twitter</a></li>
+                  <li><a href="www.facebook.com">Facebook</a></li>
                 </ul>
               </li>
             </ul>
@@ -156,12 +192,7 @@ body{
               <div id="profile" class="tab-pane fade">
                 <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
               </div>
-              <div id="dropdown1" class="tab-pane fade">
-                <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
-              </div>
-              <div id="dropdown2" class="tab-pane fade">
-                <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater. Lomo wolf viral, mustache readymade thundercats keffiyeh craft beer marfa ethical. Wolf salvia freegan, sartorial keffiyeh echo park vegan.</p>
-              </div>
+              
             </div>
 						</div>
 					</div>
